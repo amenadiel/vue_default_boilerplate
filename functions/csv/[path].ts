d09e764dba158aeb7774dee78cae23700351d44c
Felipe Figueroa <amenadiel@gmail.com>
2022-01-07 01:29:54 -0300

    deploy

---
+++ functions/csv/[path].ts
@@ -0,0 +1,20 @@
+export async function onRequestOptions({ env, request }) {
+
+    return new Response(null, {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Expose-Headers": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Range, Content-Range",
+        },
+    });
+}
+export async function onRequestGet({ env, request }) {
+
+    let response = await env.ASSETS.fetch(request)
+    response = new Response(response.body, response)
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type")
+    return response
+}
